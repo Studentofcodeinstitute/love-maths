1d610f05a02ff643c26baf5c18917f5300c03252
Studentofcodeinstitute <>
2023-07-22 17:13:26 +0000

    add event listenners for loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
+
+
+
+
+);
+
+
 function runGame() {
 
 }
@@ -29,5 +52,5 @@ function displaySubtractionQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
